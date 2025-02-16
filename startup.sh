79fda0ab6e53aa68bd2ed5be17e1fbc1a70d918a
Cheikh <115700706+saandiarem@users.noreply.github.com>
2025-02-16 14:22:05 -0500

    use gunicorn for prod

--- startup.sh
+++ startup.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Activer un environnement virtuel si nécessaire (par exemple, si vous utilisez un environnement virtuel personnalisé)
-# source /path/to/your/virtualenv/bin/activate
-
-# Lancer l'application FastAPI avec Uvicorn
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+gunicorn app.main:app -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --timeout 120
