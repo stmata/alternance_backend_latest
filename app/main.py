a9b771a9c7a1ee1b12f6b98cec61c11080fdc682
stmata <108939392+stmata@users.noreply.github.com>
2025-02-17 09:14:50 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://jobsearch-alternance.skema.edu/"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
